fa0b18801e655a4ac14ff2f7173d14a94a07f5c6
wangyan <wyvernnot@gmail.com>
2016-04-15 16:32:02 +0800

    add support for soloar wind

--- solarwind.js
+++ solarwind.js
@@ -5,7 +5,6 @@ var statsd = new StatsD('127.0.0.1', 8251);
 statsd.socket.unref();
 
 request.get('http://services.swpc.noaa.gov/products/summary/solar-wind-speed.json', (error, res, resContent)=> {
-	console.log(JSON.parse(resContent).WindSpeed);
+	console.log('solar.wind.speed = %d', JSON.parse(resContent).WindSpeed);
+	statsd.gauge('solar.wind.speed', parseFloat(JSON.parse(resContent).WindSpeed, 10))
 });
-
-
